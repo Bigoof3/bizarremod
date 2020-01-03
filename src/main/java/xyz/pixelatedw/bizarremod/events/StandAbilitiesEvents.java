eaef175e94324986d33ffae6b4e1b4c2902cec65
Wynd <wyndmaster@gmail.com>
2020-01-03 15:36:15 +0200

    Updated the magician's red alt texture, finally fixed the punch's rotation and started work on the rendering the ability cooldowns

--- src/main/java/xyz/pixelatedw/bizarremod/events/StandAbilitiesEvents.java
+++ src/main/java/xyz/pixelatedw/bizarremod/events/StandAbilitiesEvents.java
@@ -1,6 +1,11 @@
 package xyz.pixelatedw.bizarremod.events;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -42,4 +47,33 @@ public class StandAbilitiesEvents
 		}
 	}
 
+	@SuppressWarnings("resource")
+	@SubscribeEvent
+	public static void onRenderUI(RenderGameOverlayEvent.Pre event)
+	{
+		if(event.getType() == ElementType.CROSSHAIRS)
+		{
+			Minecraft mc = Minecraft.getInstance();
+			PlayerEntity player = mc.player;
+			IStandData standProps = StandDataCapability.get(player);
+			
+			if(standProps == null || player == null)
+				return;
+			
+			int posX = mc.mainWindow.getScaledWidth();
+			int posY = mc.mainWindow.getScaledHeight();
+			
+			GlStateManager.enableAlphaTest();
+			GlStateManager.enableBlend();
+			
+			GlStateManager.color4f(1, 1, 1, 0.5F);
+			
+			//System.out.println(standProps.getSecondaryAbility().getState());
+			
+			//WyRenderHelper.drawIcon(ModResourceLocations.LEFT_MOUSE_USED, posX - 40, posY - 40, 32, 32);
+						
+			GlStateManager.disableBlend();
+			GlStateManager.disableAlphaTest();
+		}
+	}
 }
--- src/main/java/xyz/pixelatedw/bizarremod/renderers/PunchRenderer.java
+++ src/main/java/xyz/pixelatedw/bizarremod/renderers/PunchRenderer.java
@@ -29,10 +29,12 @@ public class PunchRenderer extends EntityRenderer<StandProjectileEntity>
 		GlStateManager.pushMatrix();
 		GlStateManager.disableLighting();
 		
-		GlStateManager.translatef((float) x + 0.1F, (float) y + 0.35F, (float) z);
+		GlStateManager.translatef((float) x + 0.1F, (float) y, (float) z);
 		
-		GlStateManager.rotated(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 180.0F, 0.0F, 1.0F, 0.0F);
-		GlStateManager.rotated(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks - 180.0F, 1.0F, 0.0F, 0.0F);
+		GlStateManager.rotated(entity.prevRotationYaw + (entity.rotationYaw - entity.prevRotationYaw) * partialTicks - 90.0F, 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotated(entity.prevRotationPitch + (entity.rotationPitch - entity.prevRotationPitch) * partialTicks - 180.0F, 0.0F, 0.0F, 1.0F);
+			
+		GlStateManager.rotatef(90.0F, 0.0F, 1.0F, 0.0F);
 		
 		this.bindEntityTexture(entity);
 		this.punch.render(entity, (float) x, (float) y, (float) z, 0.0F, 0.0F, 0.0625F);
