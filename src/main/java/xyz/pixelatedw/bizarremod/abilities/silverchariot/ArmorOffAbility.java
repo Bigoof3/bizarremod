9e4792dfccef697bb7b2b1e3361674a52f3b04ed
Wynd <wyndmaster@gmail.com>
2020-02-09 17:09:02 +0200

    Updated Silver Chariot's model in order for it to hold it's Rapier

--- src/main/java/xyz/pixelatedw/bizarremod/abilities/silverchariot/ArmorOffAbility.java
+++ src/main/java/xyz/pixelatedw/bizarremod/abilities/silverchariot/ArmorOffAbility.java
@@ -11,27 +11,31 @@ import xyz.pixelatedw.bizarremod.entities.stands.SilverChariotEntity;
 import xyz.pixelatedw.bizarremod.helpers.StandLogicHelper;
 import xyz.pixelatedw.wypi.APIConfig.AbilityCategory;
 import xyz.pixelatedw.wypi.WyHelper;
-import xyz.pixelatedw.wypi.abilities.Ability;
+import xyz.pixelatedw.wypi.abilities.ContinuousAbility;
 
-public class ArmorOffAbility extends Ability implements IStandAbility
+public class ArmorOffAbility extends ContinuousAbility implements IStandAbility
 {
 	public ArmorOffAbility()
 	{
 		super("Armor Off", AbilityCategory.ALL);
-		this.onUseEvent = this::onUseEvent;
+		
+		this.onStartContinuityEvent = this::onStartContinuityEvent;
+		this.duringContinuity = this::duringContinuity;
+		this.onEndContinuityEvent = this::onEndContinuityEvent;
+
 	}
 	
 	@Override
 	public void renderDescription(FontRenderer fontObj, int posX, int posY)
 	{
 		this.drawLine("- " + this.getName() + " -", posX + 185, posY + 60);
-		this.drawLine(TextFormatting.GREEN + " Active", posX + 183, posY + 72);
+		this.drawLine(TextFormatting.YELLOW + " Active (Continuous)", posX + 183, posY + 72);
 		
 		this.drawLine("Increases Silver Chariot's speed and precision", posX + 190, posY + 95);
 		this.drawLine("but grealy lowers it's defense.", posX + 190, posY + 110);
 	}
 
-	protected boolean onUseEvent(PlayerEntity player)
+	private boolean onStartContinuityEvent(PlayerEntity player)
 	{
 		IStandData props = StandDataCapability.get(player);
 		StandInfo info = StandLogicHelper.getStandInfo(props.getStand());
@@ -41,6 +45,38 @@ public class ArmorOffAbility extends Ability implements IStandAbility
 			if(entity.getOwner() == player)
 			{
 				entity.removeArmor();
+				break;
+			}
+		}
+		
+		return true;
+	}
+	
+	private void duringContinuity(PlayerEntity player, int passiveTimer)
+	{
+		for(SilverChariotEntity entity : WyHelper.getEntitiesNear(player.getPosition(), player.world, 20, SilverChariotEntity.class))
+		{
+			if(entity.getOwner() == player)
+			{
+				entity.setDestructivePower('B');
+				entity.setRange('B');
+				entity.setPrecision('A');
+				
+				break;
+			}
+		}
+	}
+	
+	private boolean onEndContinuityEvent(PlayerEntity player)
+	{
+		for(SilverChariotEntity entity : WyHelper.getEntitiesNear(player.getPosition(), player.world, 20, SilverChariotEntity.class))
+		{
+			if(entity.getOwner() == player)
+			{			
+				entity.setDestructivePower('C');
+				entity.setRange('C');
+				entity.setPrecision('B');
+				
 				break;
 			}
 		}
